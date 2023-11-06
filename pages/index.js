337f6ddef8fe15ff773de3fd1e6faec743039eaa
vietdung97 <dung.ngoviet@powergatesoftware.com>
2023-11-06 22:41:12 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
-import styles from '../styles/Home.module.css';
-import React from 'react';
+import styles from "../styles/Home.module.css";
+import React from "react";
 
 export default function Home() {
-  const aRef = React.useRef(null);
-  const [search, setSearch] = React.useState('');
   React.useEffect(() => {
     setTimeout(() => {
-      withLocation();
       withATag();
-  }, 1000);
+    }, 1000);
   }, []);
-  const withLocation = () => {
-    window.location.href = 'sfclient://';
-};
-const withATag = () => {
-    var a = document.createElement('a');
-    a.href = 'sfclient://';
+
+  const withATag = () => {
+    var a = document.createElement("a");
+    a.href = "sfclient://";
     a.click();
     a.remove();
-};
+    setTimeout(() => {
+      window.close();
+    }, 100);
+  };
   return (
     <div className={styles.container}>
       {/* <a ref={aRef} href={"sfclient://app" + search} target='_blank'>Square Fitness</a> */}
       {/* <input ref={aRef} type="url" value={"sfclient://app" + search} /> */}
     </div>
-  );  
+  );
 }
