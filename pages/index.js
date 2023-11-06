81efeb54d4bff0fcb356df938d55aae9a6314447
vietdung97 <ngovietdung0112@gmail.com>
2023-11-06 20:40:51 +0700

    Fix build

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,17 @@ import React from 'react';
 
 export default function Home() {
   const aRef = React.useRef(null);
+  const [search, setSearch] = React.useState('');
   React.useEffect(() => {
+    setSearch(window.location.search);
     setTimeout(() => {
       aRef.current?.click();
       window.close();
-    }, 200)
+    }, 300)
   }, []);
   return (
     <div className={styles.container}>
-      <a ref={aRef} href={"sfclient://app" + window.location.search} target='_blank'>Square Fitness</a>
+      <a ref={aRef} href={"sfclient://app" + search} target='_blank'>Square Fitness</a>
     </div>
   );  
 }
