94d9f20624cefccaf3623c9e726f4e1032ebb6cc
vietdung97 <ngovietdung0112@gmail.com>
2023-11-06 20:37:08 +0700

    Change a href

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import styles from '../styles/Home.module.css';
 import React from 'react';
 
 export default function Home() {
+  const aRef = React.useRef<any>(null);
   React.useEffect(() => {
     setTimeout(() => {
-      const searchpath = location.search;
-      location.href = "sfclient://app" + searchpath;
+      aRef.current?.click();
       window.close();
     }, 200)
   }, []);
   return (
     <div className={styles.container}>
-
+      <a ref={aRef} href={"sfclient://app" + location.search} target='_blank'>Square Fitness</a>
     </div>
   );  
 }
